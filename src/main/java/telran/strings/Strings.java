ca546bf89a1c6b45bd5595a214a4d92d5040ec41
Андрей Крапивин <without.a.body@gmail.com>
2024-07-31 12:58:45 +0300

    Added isArithmeticExpression(), isEqualNumbersOfBrakets for HW 11

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,6 +1,18 @@
 package telran.strings;
 
+import java.util.Arrays;
+
 public class Strings {
+
+    static final String keyWords[] = { "abstract", "assert", "boolean",
+                "break", "byte", "case", "catch", "char", "class", "const",
+                "continue", "default", "do", "double", "else", "enum", "extends", "false",
+                "final", "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long", "native",
+                "new", "null", "package", "private", "protected", "public",
+                "return", "short", "static", "strictfp", "super", "switch",
+                "synchronized", "this", "throw", "throws", "transient", "true",
+                "try", "void", "volatile", "while" };
 
     public static String firstName() {
         // regex for strings starting with capitallet letter and
@@ -19,6 +31,82 @@ public class Strings {
         // regular expression for testing syntax of Java veriable names
         // only ASCII symbols are allowed
         return "([a-zA-Z_$][a-zA-Z0-9_$]+)|([a-zA-Z$])";
+    }
+
+    public static String number0_300() { 
+        return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+    }
+
+    public static String ipv4Octet() {
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+
+    public static String ipV4Address() {
+        String octetExpr = ipv4Octet();
+        return String.format("%s(\\.%s){3}", octetExpr, octetExpr);
+    }
+
+    public static String stringWithJavaNames(String names) {
+        String [] tokens = names.split("\\s+");
+        int i = getJavaNameIndex(tokens, -1);
+        String res = "";
+        if (i >= 0) {
+            res = tokens[i];
+        }
+        while ((i = getJavaNameIndex(tokens, i)) > 0) {
+            res += " " + tokens[i];
+        }
+        return res;
+    }
+
+    private static int getJavaNameIndex(String[] tokens, int i) {
+        i++;
+        while (i < tokens.length && !isJavaName(tokens[i])) {
+            i++;    
+        }
+        return i < tokens.length ? i : -1;
+    }
+
+    private static boolean isJavaName(String string) {
+        return string.matches(javaVariable()) && Arrays.binarySearch(keyWords, string) < 0;
+    }
+
+    public static boolean isArithmeticExpression(String expr) {
+        //1. brackets 
+        //right position of open / close bracket is matter of regex
+        //matching between open and close bracket is matter of the method you are supporsed to write
+        //based on a counter. If counter is negative - no matching; if at ending up going through a string
+        //matching may be only in one case: at the  ending up of going the counter will be 0
+        //Operator - regular expression for one out of 4 artithmetic operators [*/+-]
+        //Operand may be either Java variable name or number (better any)
+
+        boolean res = true;
+        
+        if (!isEqualNumbersOfBrakets(expr) || expr.strip().length() == 0) {
+            res = false;
+        } else {
+            expr = expr.replaceAll("\\s+", "");
+            String[] arrayOperation = expr.split("[-+/*]");
+            int i = 0;
+            while (i < arrayOperation.length && res) {
+                  if (arrayOperation[i].matches("[\\)].*||.*[\\($]||.*[a-zA-Z0-9_$][\\\\(].*||.*[\\\\)][a-zA-Z0-9_$].*")) {
+                    res = false;    
+                } else {
+                    String stringWihtoutBrakets = arrayOperation[i].replaceAll("[\\(\\)]+", "");
+                    if (!isJavaName(stringWihtoutBrakets) && !stringWihtoutBrakets.matches("\\d+")) {
+                        res = false;
+                    }
+                }
+                i++;
+            }
+        } 
+
+        return res;
+    }
+
+    private static boolean isEqualNumbersOfBrakets(String expr) {
+        return (expr.length() - expr.replaceAll("[\\(]+", "").length()) 
+            == (expr.length() - expr.replaceAll("[\\)]+", "").length());    
     }
 
 }
--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -1,5 +1,6 @@
 package telran.strings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static telran.strings.Strings.*;
@@ -25,5 +26,128 @@ public class RegularExpressionsTest {
         assertFalse(Pattern.matches(javaVariable(), "nam.e"));
         assertFalse(Pattern.matches(javaVariable(), "[1"));
         assertFalse(Pattern.matches(javaVariable(), "_"));
+    }
+
+    @Test
+    void number0_300TrueTest() {
+        String regex = Strings.number0_300();
+        assertTrue("0".matches(regex));
+        assertTrue("300".matches(regex));
+        assertTrue("250".matches(regex));
+        assertTrue("25".matches(regex));
+        assertTrue("12".matches(regex));
+        assertTrue("299".matches(regex));
+        assertTrue("1".matches(regex));
+    }
+
+    @Test
+    void number0_300FalseTest() {
+        String regex = Strings.number0_300();
+        assertFalse("00".matches(regex));
+        assertFalse("301".matches(regex));
+        assertFalse("01".matches(regex));
+        assertFalse("00".matches(regex));
+        assertFalse("1(".matches(regex));
+        assertFalse("1000".matches(regex));
+        assertFalse(" 20".matches(regex));
+        assertFalse("1001".matches(regex));
+    }
+
+    @Test
+    void ipv4OctetTrueTest() {
+        String regex = Strings.ipv4Octet();
+        assertTrue("0".matches(regex));
+        assertTrue("00".matches(regex));
+        assertTrue("000".matches(regex));
+        assertTrue("10".matches(regex));
+        assertTrue("100".matches(regex));
+        assertTrue("255".matches(regex));
+        assertTrue("199".matches(regex));
+        assertTrue("249".matches(regex));
+    }
+
+    @Test
+    void ipv4OctetFalseTest() {
+        String regex = Strings.ipv4Octet();
+        assertFalse("0000".matches(regex));
+        assertFalse("t".matches(regex));
+        assertFalse("-1".matches(regex));
+        assertFalse("1111".matches(regex));
+        assertFalse("0001".matches(regex));
+        assertFalse("256".matches(regex));
+        assertFalse("300".matches(regex));
+        assertFalse("*".matches(regex));
+        assertFalse("1 ".matches(regex));
+    }
+
+    @Test
+    void ipV4AddressTrueTest() {
+        
+        String regex = Strings.ipV4Address();
+        assertTrue("0.0.0.0".matches(regex));
+        assertTrue("255.255.255.255".matches(regex));
+    }
+
+    @Test
+    void ipV4AddressFalseTest() {
+        
+        String regex = Strings.ipV4Address();
+        assertFalse("0.0.0".matches(regex));
+        assertFalse("0.0.0+0".matches(regex));
+        assertFalse("0".matches(regex));
+        assertFalse("0.-".matches(regex));
+        assertFalse("0.0.0*0".matches(regex));
+        assertFalse("0.0.0 0".matches(regex));
+    }
+
+    @Test
+    void stringWithJavaNamesTest() {
+        String names = "123 1a _ abs int enum null lmn";
+        String expected = "abs lmn";
+        assertEquals(expected, Strings.stringWithJavaNames(names));
+    }
+
+    @Test
+    void isArithmeticExpressionTrueTest() {
+        String expr1 = "((a + x) / 5) + 15";
+        assertTrue(isArithmeticExpression(expr1));
+        String expr2 = "per / name + (10 + 2) * (10 + 5)";
+        assertTrue(isArithmeticExpression(expr2));
+        String expr3 = "7 - 4 + 8 * (tr + nm)";
+        assertTrue(isArithmeticExpression(expr3));
+        String expr4 = "(  a + (b * c )  ) - ( (   (   d) / e ) )";
+        assertTrue(isArithmeticExpression(expr4));
+        String expr5 = "((((a))))";
+        assertTrue(isArithmeticExpression(expr5));
+    }
+
+    @Test
+    void isArithmeticExpressionFalseTest() {
+        String expr1 = "((a + x) / 5 + 15";
+        assertFalse(isArithmeticExpression(expr1));
+        String expr2 = "3 + double";
+        assertFalse(isArithmeticExpression(expr2));
+        String expr3 = "(a (+) b)";
+        assertFalse(isArithmeticExpression(expr3));
+        String expr4 = "per / name + )10 + 2( * (10 + 5)";
+        assertFalse(isArithmeticExpression(expr4));
+        String expr5 = "5 +- 6";
+        assertFalse(isArithmeticExpression(expr5));
+        String expr6 = "5 ++ 6";
+        assertFalse(isArithmeticExpression(expr6));
+        String expr7 = ")a + b(";
+        assertFalse(isArithmeticExpression(expr7));
+        String expr8 = "а b + 3";
+        assertFalse(isArithmeticExpression(expr8));
+        String expr9 = "7 - () + 4";
+        assertFalse(isArithmeticExpression(expr9));
+        String expr10 = "7 - (b)4";
+        assertFalse(isArithmeticExpression(expr10));
+        String expr11 = "";
+        assertFalse(isArithmeticExpression(expr11));
+        String expr12 = "    ";
+        assertFalse(isArithmeticExpression(expr12));
+        String expr13 = "7 - 4(b)";
+        assertFalse(isArithmeticExpression(expr13));
     }
 }
